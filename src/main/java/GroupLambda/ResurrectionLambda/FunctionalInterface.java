4d82dc93c3212a895d5ad4fa1b14210d1dd13fcf
sumit raut <mytrade1086@gmail.com>
2020-06-26 10:19:55 +0530

    added first code

--- src/main/java/GroupLambda/ResurrectionLambda/FunctionalInterface.java
+++ src/main/java/GroupLambda/ResurrectionLambda/FunctionalInterface.java
@@ -1,27 +1,34 @@
 package GroupLambda.ResurrectionLambda;
 
 public class FunctionalInterface {
-	
-	
-	public interface  demo {
-		
-		public void  print (String name); //Single Abstract Method
-		default void  defaultDemo (String name) {		
-			System.out.println("Printing name "+name);	
+
+	public interface demo {
+
+		public void print(String name); // Single Abstract Method
+		default void defaultDemo(String name) {
+			System.out.println("Printing name " + name);
 		}
 	}
-	
-	
+
+	public interface newInterface {
+		public void newMethod(String name); // Single Abstract Method
+	}
+
 	public static void main(String[] args) {
 		
+		//1.Using Labmbda
+		demo dm = (string) -> System.out.println("Implemented Functional Interface " + string);
+		dm.print("Running");
 		
 		
+		//2. Anonymous inner type
+		newInterface nm = new newInterface() {
+			@Override
+			public void newMethod(String name) {
+				System.out.println("new methid overridden " + name);
+			}
+		};		nm.newMethod("sumit");
+
 	}
-	
-	
-	
-	
-	
-	
 
 }
